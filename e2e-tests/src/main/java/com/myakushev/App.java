af9d62384cd72ece5078f7abafd0a335db72b721
yakushev <mikhail.yakushev@thebricks.com>
2024-11-03 02:25:28 +0300

    debug 6

--- e2e-tests/src/main/java/com/myakushev/App.java
+++ e2e-tests/src/main/java/com/myakushev/App.java
@@ -5,7 +5,7 @@ import com.microsoft.playwright.*;
 public class App {
     public static void main(String[] args) {
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(50));
+            Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true).setSlowMo(50));
             Page page = browser.newPage();
 //            page.navigate("http://playwright.dev");
             page.navigate("http://localhost:4200/");
