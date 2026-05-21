ecc2eb5b5d181519839da534f4e23e0e1303eb35
yakushev <mikhail.yakushev@thebricks.com>
2026-05-21 13:11:29 +0300

    fix xpath initialization - do it lazy

--- TestFrameworkNB/src/main/java/com/myakushev/ui/HomePage.java
+++ TestFrameworkNB/src/main/java/com/myakushev/ui/HomePage.java
@@ -18,9 +18,15 @@ public class HomePage extends Page {
     private SelenideElement saveButton = popup.$x(".//button[text()='Save']");
     private SelenideElement closeButton = popup.$x(".//button[text()='Close']");
 
-    private SelenideElement mainCard = $(byXpath(".//div[@class='main-card']"));
-    private ElementsCollection kanbanItem = mainCard.$$x(".//a[@class='mat-list-item']");
+//    private SelenideElement mainCard = $(byXpath(".//div[@class='main-card']"));
+//    private ElementsCollection kanbanItem = mainCard.$$x(".//a[@class='mat-list-item']");
 
+
+    private SelenideElement mainCard = $(byXpath(".//div[@class='main-card']"));
+
+    private ElementsCollection kanbanItems() {
+        return mainCard.$$x(".//a[contains(@class,'mat-list-item')]");
+    }
 
     public HomePage open() {
         Selenide.open("http://localhost:4200/");
@@ -46,7 +52,7 @@ public class HomePage extends Page {
     }
 
     public KanbanPage openKanbanBoardWitName(String kanbanName) {
-        kanbanItem
+        kanbanItems()
                 .findBy(text(kanbanName))
                 .shouldBe(clickable)
                 .click();
